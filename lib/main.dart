c2361122f956b6510034eec718bfc555155e5f8c
am_tech <william.souza.sena.alves93@gmail.com>
2026-05-25 21:30:12 -0400

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -72,6 +72,7 @@ class _MyHomePageState extends State<MyHomePage> {
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
+          
         ),
       ),
       floatingActionButton: FloatingActionButton(
